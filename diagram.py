68dddcb7230089d4eb5100f8087f7b70afc732b7
kaleolson <Karl1.Olsson1@outlook.com>
2025-02-03 00:00:14 +0100

    1

---
+++ diagram.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+pivot_df = pd.read_csv(r"C:\Users\Karl1\Desktop\TUC\Data-Science2\Inlamning2-main\Inlamning2\Pivot_LanKostnader.csv", sep=';', encoding='utf-8')
+
+
+#Histogram över hur länkostnaderna är fördelade
+plt.hist(pivot_df["Skilland mellan 2017 och 2019"], bins=10)
+plt.xlabel("Skillnad mellan 2017 och 2019")
+plt.ylabel("Antal län")
+plt.title("Fördelning av skillnader")
+plt.show()
+#--------------------------------------------------------------------------------------------------------
+#Linje diagram per län
+# Välj ett län att visa
+lan = "AB"  # Ändra detta för att välja ett annat län
+lan_data = pivot_df[pivot_df["Län"] == lan]
+
+#Välj vilka värden som ska visas på x-axeln
+x_values = [2017,2018,2019]
+# Skapa linjediagram
+plt.plot([2017.0, 2018.0, 2019.0], lan_data.iloc[0, 2:])
+
+plt.xlabel("År")
+plt.ylabel("Värde")
+plt.title(f"Utveckling av värden för län {lan} mellan 2017 och 2019")
+plt.xticks(x_values) #visar x-värden som valdes innan 
+plt.show()
+#--------------------------------------------------------------------------------------------------------
+#Scatterplot
+län_namn = list(pivot_df.keys())
+values_2017 = []
+for l in län_namn:
+    value = pivot_df[l][0]
+    values_2017.append(value)
+
+values_2019 = []
+for l in län_namn:
+    value = pivot_df[l][1]
+    values_2019.append(value)
+
+# Skapa scatterplot
+plt.scatter(values_2017, values_2019, color='blue')
+
+""" # Lägg till etiketter för varje punkt
+for i, namn in enumerate(län_namn):
+    plt.text(values_2017[i]*1.01, values_2019[i]*0.99, namn, fontsize=9)
+ """
+plt.xlabel('Värde 2017')
+plt.ylabel('Värde 2019')
+plt.title('Scatterplot: Värden per län 2017 vs 2019')
+plt.show()
--- kodblock.py
+++ kodblock.py
@@ -1,21 +1,38 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import math
 #Importerarat: Länsstyrelsernas lokalytor och lokalkostnader fr o m 2017
 #behövde encoda till latin-1 för att läsa filen
 df = pd.read_csv(r"C:\Users\Karl1\Desktop\TUC\Data-Science2\Lanlokalkostnader.csv", sep=';', encoding='utf-8')
 
-#print("The DataFrame :")
-#print(df.head()) 
+#Kollar om det är några rader som är null
+print("Kolumner som har nullvärden:")
+print(df.isnull().sum())
+#kollar om det finns några nullvärden i kolumnen "Län"
+print("isNull")
+isNull = df["År"].isnull().sum()
+print(isNull)
 
-#print(df["residens (m2)"]) 
-
-#df["Total Sales"] = df["`Lokalkostnader (kr)`"] / df["`Kontorslokalyta (m2)`"]
-""" print("isNull")
-isNull = df["A"].isnull().sum()
-print(isNull) """
-
-#df.dropna(inplace=True)
-#new_df = df.dropna()
-df["Län"].fillna("K", inplace = True)
+#Fyller i det år som saknades
+df["År"] = df["År"].fillna(2019)
 print(df.to_string)
 
-#df.to_csv("NyLanlokalkostnader.csv", index=False,sep='|', encoding='latin-1')
+#Skapa en pivot-tabell för att sammanställa värden
+pivot_df = df.pivot_table(values="Summa lokalkostnader (kr)", index = "Län", columns = "År", aggfunc = "sum")
+
+#Ändrar datan i kolumneran till float och gör om "," till "."
+tempKol = [2017,2018,2019]
+pivot_df[tempKol] = pivot_df[tempKol].apply(lambda x: x.str.replace(',', '.').astype(float))
+
+pivot_df["Skilland mellan 2017 och 2019"] = round(pivot_df[2019.0] - pivot_df[2017.0])
+
+print("Totala års kostnader på län i en pivot:")
+print(pivot_df)   
+
+#skapa en ny fil för pivot-datan. behövde använda utf-8-sig för att få fram svenska tecken
+pivot_df.to_csv("Pivot_LanKostnader.csv",sep=';', encoding='utf-8-sig')
+
+
+#df.to_csv("NyLanlokalkostnader.csv", index=False,sep='|', encoding='utf-8')
+
